884081d67cfc4b377f05eecd8745dfea824fbc76
Ryan19900529 <ab00999819@gmail.com>
2024-11-01 20:52:14 -0700

    frontend start

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Fragment } from "react";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import "./App.css";
+
+const App = () => (
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Landing />} />
+      </Routes>
+      <section className='container'>
+        <Routes>
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </section>
+    </Fragment>
+  </Router>
+);
+export default App;
